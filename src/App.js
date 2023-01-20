e30d9e00bbfd037d15705e68d1ed8952d02db56f
salviuez <salviuez19@gmail.com>
2023-01-20 16:17:24 +0530

    Data from api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { useState, useEffect } from 'react';
 
+// const API = "http://localhost:4000/mobiles";
+const API = "https://vercel.com/salviuez/mdb";
+
 function App() {
   return (
     <div className="App">
@@ -11,28 +14,16 @@ function App() {
 }
 
 function PhoneList() {
-  const mobiles = [{
-    model: "OnePlus 9 5G",
-    img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
-    company: "Oneplus"
-  },
-  {
-    model: "Iphone 13 mini",
-    img:
-      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1645572315986",
-    company: "Apple"
-  },
-  {
-    model: "Samsung s21 ultra",
-    img: "https://m.media-amazon.com/images/I/81kfA-GtWwL._SY606_.jpg",
-    company: "Samsung"
-  },
-  {
-    model: "Xiomi mi 11",
-    img: "https://m.media-amazon.com/images/I/51K4vNxMAhS._AC_SX522_.jpg",
-    company: "Xiomi"
-  }
-  ];
+  const [mobiles, setMobiles] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}/mobiles`)
+      .then((data) => data.json())
+      .then((mbs) => setMobiles(mbs));
+
+  }, []);
+
+
   return (
     <div className="phone-list-container">
       {mobiles.map((mb) => (
